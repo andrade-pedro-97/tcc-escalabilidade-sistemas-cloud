236fe2f28a64151a0d3ce464fed52ed9d7054eee
Brundes <pedro.pac97@gmail.com>
2025-06-14 15:10:26 -0300

    Integrando a aplicação com banco PostgreSQL na AWS RDS

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlmodel import create_engine, Session
 
 # URL para conexão com o banco de dados PostgreSQL (Uso local)
 # TO DO -> trocar para a URL da AWS
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/meubanco"
+# DATABASE_URL = "postgresql://postgres:123456@localhost:5432/meubanco"  -- Usado Localmente
+DATABASE_URL = "postgresql://postgres:12345678@tcc-db.cwn2um4u0dye.us-east-1.rds.amazonaws.com:5432/postgres"
 
 # Criação de motor de conexão com o banco
 engine = create_engine(DATABASE_URL, echo=True)
